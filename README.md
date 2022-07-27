3bf1eb9ca849f2cb52fec2a45c4ed9fc74b37d75
NotADev9000 <notadev9000@gmail.com>
2022-07-27 19:13:38 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RMMZ CoreScripts
+RPG Maker MZ scripts split into separate files.
+Also contains the script to split the core files.
+
+## To use
+1. move 'splits.js' into your projects 'js' folder.
+2. create folders for each rmmz file e.g. 'rmmz_core', 'rmmz_manager' etc.
+3. run 'node splits.js' on command line
---
+++ js/splits.js
@@ -0,0 +1,40 @@
+/*: ==========================================================================
+* ############################################################################
+*
+* Plugin: Split RMMZ.js files
+* Author: DekitaRPG [dekitarpg.com] (dekitarpg@gmail.com)
+*
+* ############################################################################
+* ============================================================================
+* 1. move this file into your projects 'js' folder.
+* 2. create folders for each rmmz file e.g. 'rmmz_core', 'rmmz_manager' etc.
+* 3. run 'node splits.js' on command line
+*
+* Modified by NotADev to split the files into 'rmmz_xxx' folders
+*/
+const splitter = "//-----------------------------------------------------------------------------";
+const fs = require('fs');
+const path = require('path');
+console.log('#scanning-all-files:..')
+const cdirectoryFiles = fs.readdirSync('.');
+const commandFiles = cdirectoryFiles.filter(f => {
+    return f.startsWith('rmmz_') && f.endsWith('.js')
+});
+console.log('#scanning-rmmz-files:', commandFiles)
+for (const filename of commandFiles) {
+    fs.readFile(path.join('.', filename), 'utf8', (err, data) => {
+        if (err) throw err;
+        const split_classes = data.split(splitter);
+        for (const [i, maybeclass]  of split_classes.entries()) {
+            const tempname = filename.replace('.js','');
+            const matches = maybeclass.match(/(function) \w+/);
+            const classname = matches ? matches[0].replace('function','').trim() : `unknown-class-${i}`;
+            const newpath = `/${tempname}/${classname}.js`;
+            const newfilepath = path.join('.', newpath);
+            fs.writeFile(newfilepath, maybeclass, (werr, wdata) =>{
+                if (werr) throw werr;
+                console.log('wrote', newfilepath);
+            })
+        }
+    });
+}
